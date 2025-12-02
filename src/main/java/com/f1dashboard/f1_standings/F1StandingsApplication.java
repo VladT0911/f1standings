cc8be8cc953a02d66a5cb068e3200909e5eb82ca
VladT0911 <vlad.tataru@student.upt.ro>
2025-12-02 18:19:49 +0200

    Add files via upload

---
+++ src/main/java/com/f1dashboard/f1_standings/F1StandingsApplication.java
@@ -0,0 +1,13 @@
+package com.f1dashboard.f1_standings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1StandingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1StandingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1dashboard/f1_standings/F1StandingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1dashboard.f1_standings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1StandingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
